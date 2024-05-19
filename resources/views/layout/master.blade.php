362bc3b661f310a3ab929ca9b53d61d25cbcc245
richandcapable <mhmmdnabilm@gmail.com>
2024-05-19 13:35:01 +0800

    Progress Bar Fix

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -28,8 +28,11 @@
 
   <!-- Template Main CSS File -->
   <link href="../../assets/css/styleadmin.css" rel="stylesheet">
-  <script src ="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/parsley.js/2.9.2/parsley.min.js" integrity="sha512-eyHL1atYNycXNXZMDndxrDhNAegH2BDWt1TmkXJPoGf1WLlNYt08CSjkqF5lnCRmdm3IrkHid8s2jOUY4NIZVQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
 
 </head>
 
